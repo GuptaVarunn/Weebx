677128508d50b27cbbc7e86dcfff65b6ad3f1bd7
SAHIL <2022.girish.chougule@ves.ac.in>
2025-03-27 11:27:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import React from "react";
+import { BrowserRouter,Routes } from "react-router-dom";
+export default function App() {
+  return (
+    <>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    <BrowserRouter>
+    <Routes>
+      <Route />
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
